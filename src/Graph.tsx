167e004eee2f17b7d699fa1bfb010a65d7ebaec1
josiahdc <josiah.chapman@gmail.com>
2022-10-05 13:07:01 -0700

    The task is done. The explanation was provided in a separate file

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import {Table, TableData} from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -23,10 +23,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      price_abc: 'float', // prices are necessary to calculate the ratio
+      price_def: 'float',
+      ratio: 'float',  // We added 'ratio' to track stock's ratios
+      timestamp: 'date', // Stocks are tracked according to time
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float', // We added 'trigger' to track the moment when upper/lower bounds are crossed
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -36,23 +39,25 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+      elem.setAttribute('row-pivots', '["timestamp"]'); // row-pivots tales care about x-axis. This allows us to depict each datapoint based on its timestamp.
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound","trigger_alert"]'); // 'columns' allows to concentrate on a certain part of a datapoint’s data along the y-axis
+      elem.setAttribute('aggregates', JSON.stringify({ // 'aggregates' allows us to handle the duplicate data and encompass them into one data point.
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+        this.table.update([
+            DataManipulator.generateRow(this.props.data),
+        ] as unknown as TableData);
     }
   }
 }
